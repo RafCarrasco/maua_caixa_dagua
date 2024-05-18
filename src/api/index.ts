ac069433216e6f4d7f531f33bfdd92260159ef3d
Yuji Arima <yujiarima@hotmail.com>
2024-05-18 10:31:11 -0300

    mock : artesian well,hidrometer and watertank

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,21 +1,41 @@
-import {  WaterTanks } from "@/interface";
 import { env } from "@/env";
-import { mock } from "./mocks/get-waterbox-mock";
-import { useWaterTank } from "@/contexts/water-tank-provider";
+import { mockProps } from "./mocks";
+import { mockWaterTanks } from "./mocks/get-waterbox-mock";
+import { mockHidrometers } from "./mocks/get-hidrometer";
+import { mockArtesianWells } from "./mocks/get-artesian-well";
+import { useApplication } from "@/contexts";
 
-export function fetchData(): WaterTanks | [] {
-  const { waterTanks } = useWaterTank();
-  
+export function fetchData(): mockProps {
+  const { artesianWells, hidrometers, waterTanks } = useApplication();
+
   if (env.MODE === "development") {
-    return waterTanks;
-
-  } 
-  else{
-    return mock;
+    return {
+      artesianWell: artesianWells,
+      hidrometer: hidrometers,
+      waterTank: waterTanks
+    };
 
   }
-  
+  else {
+    return {
+      waterTank: mockWaterTanks,
+      artesianWell: mockArtesianWells,
+      hidrometer: mockHidrometers,
+    };
+
+  }
+
 }
-export function getWaterTankByLevel(level: string) {
-  return fetchData().find((item) => item.level === level);
+export function getWaterTankById(waterTankId: string) {
+  return fetchData().waterTank.find((waterTank) => waterTank.watertTankId === waterTankId);
 }
+
+export function getHidrometerById(hidrometerId: string) {
+  return fetchData().hidrometer.find((hidrometer) => hidrometer.hidrometerId === hidrometerId);
+}
+
+export function getArtesianWellById(artesianWellId: string) {
+  return fetchData().artesianWell.find((artesianWell) => artesianWell.artesianWellId === artesianWellId);
+}
+
+
