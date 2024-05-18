c3ca50e022cfd55ffc1b84a8481f45d91ccdacd7
Yuji Arima <yujiarima@hotmail.com>
2024-05-18 10:32:15 -0300

    update baseURL

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -2,6 +2,6 @@ import axios from 'axios'
 
 
 export const api = axios.create({
-  baseURL:'http://localhost:3000/api',
+  baseURL:'http://localhost:3000/api/timeseries/v0.5/smartcampusmaua',
   
 })
